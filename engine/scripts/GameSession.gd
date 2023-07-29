09d0b9e714b5b58ef68e31707b98fdeb2e658a14
Ádám Liszkai <adam@liszkai.dev>
2023-07-29 04:00:27 +0200

    feat: add engine session key mapping and helpers

--- engine/scripts/GameSession.gd
+++ engine/scripts/GameSession.gd
@@ -1,5 +1,5 @@
 class_name GameSession
-extends Container
+extends Node
 
 var settlement: Settlement
 var quest_board: QuestBoard
@@ -9,6 +9,22 @@ var money: int = 1000
 func _ready() -> void:
 	if quest_board is QuestBoard:
 		quest_board.connect("quest_completed", _on_money_earned.bind(self))
+
+	GameTick.start()
+
+
+func _unhandled_key_input(event) -> void:
+	if event.is_action_pressed("game_pause"):
+		GameTick.speed = GameTick.TurnSpeed.PAUSE
+	if event.is_action_pressed("game_speed_normal"):
+		GameTick.speed = GameTick.TurnSpeed.NORMAL
+	if event.is_action_pressed("game_speed_double"):
+		GameTick.speed = GameTick.TurnSpeed.DOUBLE
+	if event.is_action_pressed("game_speed_triple"):
+		GameTick.speed = GameTick.TurnSpeed.TRIPLE
+
+	if OS.is_debug_build() and event.is_action_pressed("game_speed_ultra"):
+		GameTick.speed = GameTick.TurnSpeed.ULTRA
 
 
 func _on_money_earned(_quest: Quest, earnings: int, _event) -> void:
